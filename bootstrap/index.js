0026ca5c66d12c1de9678d50296c77c8475833ee
SonMichael <71422284+SonMichael@users.noreply.github.com>
2022-11-04 08:38:38 +0000

    Deploying to ci-cd from develop @ 32c562b0085860a4b9d07575c38e038f2f50b698 🚀

--- bootstrap/index.js
+++ bootstrap/index.js
@@ -1,5 +1,10 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const dotenv_1 = require("dotenv");
-(0, dotenv_1.config)();
+const path_1 = __importDefault(require("path"));
+const pathConfig = path_1.default.join(process.cwd(), '.env');
+(0, dotenv_1.config)({ path: pathConfig });
 //# sourceMappingURL=index.js.map
